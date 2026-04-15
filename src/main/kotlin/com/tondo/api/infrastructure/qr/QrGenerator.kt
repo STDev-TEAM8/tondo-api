583d91bc4fd90e4035d68aa6bff8732f3b2fb0d7
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2026-04-16 01:52:36 +0900

    <feat> QR Generator 구현

--- src/main/kotlin/com/tondo/api/infrastructure/qr/QrGenerator.kt
+++ src/main/kotlin/com/tondo/api/infrastructure/qr/QrGenerator.kt
@@ -1,8 +1,24 @@
 package com.tondo.api.infrastructure.qr
 
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.client.j2se.MatrixToImageWriter
+import com.google.zxing.qrcode.QRCodeWriter
 import org.springframework.stereotype.Component
+import java.io.ByteArrayOutputStream
 
 @Component
 class QrGenerator {
+    fun generate(qrGenerationCommand: QrGenerationCommand) : ByteArray {
+        // 1. QR 코드 스펙으로 텍스트 인코딩
+        val qrCodeWriter = QRCodeWriter()
+        val bitMatrix = qrCodeWriter.encode(
+            qrGenerationCommand.content, BarcodeFormat.QR_CODE, qrGenerationCommand.width, qrGenerationCommand.height
+        )
 
+        // use 블록의 결과값이 통째로 반환되도록 return을 앞으로 뺍니다.
+        return ByteArrayOutputStream().use { outputStream ->
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream)
+            outputStream.toByteArray() // 마지막 줄의 결과가 use 블록의 반환값이 됨
+        }
+    }
 }
