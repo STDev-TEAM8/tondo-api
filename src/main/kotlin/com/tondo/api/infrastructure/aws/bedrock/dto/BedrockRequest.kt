30b288432b2e1256cbe3ac0013356108300af97f
HP657 <junlee5070@gmail.com>
2026-04-16 20:05:55 +0900

    <feat> json 구조화 모듈 가져와 값 넣는 DTO 수정

--- src/main/kotlin/com/tondo/api/infrastructure/aws/bedrock/dto/BedrockRequest.kt
+++ src/main/kotlin/com/tondo/api/infrastructure/aws/bedrock/dto/BedrockRequest.kt
@@ -1,7 +1,11 @@
 package com.tondo.api.infrastructure.aws.bedrock.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class BedrockRequest(
+    @JsonProperty("anthropic_version")
     val anthropicVersion: String = "bedrock-2023-05-31",
+    @JsonProperty("max_tokens")
     val maxTokens: Int = 1024,
     val messages: List<BedrockMessage>
 )
