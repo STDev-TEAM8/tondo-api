ca4ffe89da7c92866056e14599ab6054b1f07730
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2026-04-17 00:16:14 +0900

    <fix> UserService 메서드 버그 픽스

--- src/main/kotlin/com/tondo/api/service/UserService.kt
+++ src/main/kotlin/com/tondo/api/service/UserService.kt
@@ -12,10 +12,10 @@ class UserService (
     private val passwordEncoder: PasswordEncoder
 ) {
     fun getOrCreateUser(request: SignupRequest): User {
+
         if (request.password.length != 4) {
             throw IllegalArgumentException("비밀번호는 반드시 4글자여야 합니다.")
         }
-
         val encodedPassword = passwordEncoder.encode(request.password)
 
         return userRepository.save(User(username = request.username, password = encodedPassword))
