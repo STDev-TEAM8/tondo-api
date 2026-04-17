cbd0f5ba26093965dfe402e5f860243943b5936a
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2026-04-17 11:27:51 +0900

    <feat> Spring Actuator 의존성 추가 : CICD Health-check & 예정된 모니터링 대시보드 고려 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("org.postgresql:postgresql")
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
